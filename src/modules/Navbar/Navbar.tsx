411e66cccfd6b42e89e81baf5d9a9b4a3ed0e197
Daniel Bogucki <daniel.bogucki14@gmail.com>
2023-10-26 12:08:37 +0200

    Added more Navbar styles

--- src/modules/Navbar/Navbar.tsx
+++ src/modules/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import clsx from "clsx";
+import { Link, useLocation } from "react-router-dom";
 import Heart from "~/media/heart.svg";
 import Home from "~/media/home.svg";
 import Movie from "~/media/movie.svg";
@@ -7,15 +8,25 @@ import Tv from "~/media/tv.svg";
 import { paths } from "~/utils/paths";
 
 export const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="bg-black px-4 py-8 text-black">
       <ul className="flex justify-around gap-10 md:w-10 md:flex-col md:justify-start">
-        <li className="hover:opacity-80">
+        <li
+          className={clsx("flex justify-center hover:opacity-80", {
+            "scale-125": location.pathname === "/",
+          })}
+        >
           <Link to={paths.index}>
             <img alt="home" src={Home} aria-label="home" className="h-6 w-6" />
           </Link>
         </li>
-        <li className="hover:opacity-80">
+        <li
+          className={clsx("flex justify-center hover:opacity-80", {
+            "scale-125": location.pathname.includes("movie"),
+          })}
+        >
           <Link to={paths.movies}>
             <img
               alt="movie"
@@ -25,12 +36,20 @@ export const Navbar = () => {
             />
           </Link>
         </li>
-        <li className="hover:opacity-80">
+        <li
+          className={clsx("flex justify-center hover:opacity-80", {
+            "scale-125": location.pathname.includes("tv"),
+          })}
+        >
           <Link to={paths.tv}>
             <img alt="tv" src={Tv} aria-label="tv" className="h-6 w-6" />
           </Link>
         </li>
-        <li className="hover:opacity-80">
+        <li
+          className={clsx("flex justify-center hover:opacity-80", {
+            "scale-125": location.pathname.includes("search"),
+          })}
+        >
           <Link to={paths.search}>
             <img
               alt="search"
@@ -40,7 +59,11 @@ export const Navbar = () => {
             />
           </Link>
         </li>
-        <li className="hover:opacity-80">
+        <li
+          className={clsx("flex justify-center hover:opacity-80", {
+            "scale-125": location.pathname.includes("likes"),
+          })}
+        >
           <Link to={paths.likes}>
             <img
               alt="likes"
